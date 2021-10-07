4e1ed8bf70f4c863c0bdda995bf5cfaedf868d7d
Sehyeon Oh <76672259+RNCST@users.noreply.github.com>
2021-10-07 21:14:30 +0900

    Create update_readme.py

---
+++ update_readme.py
@@ -0,0 +1,21 @@
+import feedparser
+
+url = "https://rrrrrrrrrnnn.tistory.com/feed"
+rss_feed = feedparser.parse(url)
+
+MAX_POST_NUM = 10 
+latest_blog_post_list = "" 
+MAX_POST_NUM = 10 
+for idx, feed in enumerate(rss_feed['entries']):
+   if idx > MAX_POST_NUM: 
+     break 
+     
+     feed_date = feed['published_parsed'] 
+
+     latest_blog_post_list += f"[{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n" 
+
+     markdown_text = "" 
+
+     readme_text = f"{markdown_text}{latest_blog_post_list}" 
+     
+     with open("README.md", 'w', encoding='utf-8') as f: f.write(readme_text)
